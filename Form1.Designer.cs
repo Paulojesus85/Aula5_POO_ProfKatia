1adf88f2dd307ca70ffe40875b8d75df0aa9bca8
Prof. Paulo Jesus <paulo.historia.usp@gmail.com>
2022-09-17 18:03:55 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Aula5_RA6322501
+{
+    partial class Frm_Jogador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lbl_Nome = new System.Windows.Forms.Label();
+            this.Lbl_Peso = new System.Windows.Forms.Label();
+            this.Lbl_Altura = new System.Windows.Forms.Label();
+            this.Lbl_Idade = new System.Windows.Forms.Label();
+            this.Lbl_Camisa = new System.Windows.Forms.Label();
+            this.Lbl_Time = new System.Windows.Forms.Label();
+            this.Txt_Nome = new System.Windows.Forms.TextBox();
+            this.Txt_Peso = new System.Windows.Forms.TextBox();
+            this.Txt_Altura = new System.Windows.Forms.TextBox();
+            this.Txt_Idade = new System.Windows.Forms.TextBox();
+            this.Txt_Camisa = new System.Windows.Forms.TextBox();
+            this.Txt_Time = new System.Windows.Forms.TextBox();
+            this.Btn_Salvar = new System.Windows.Forms.Button();
+            this.Btn_Pesquisa = new System.Windows.Forms.Button();
+            this.Txt_Pesquisar = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Lbl_Nome
+            // 
+            this.Lbl_Nome.AutoSize = true;
+            this.Lbl_Nome.Location = new System.Drawing.Point(13, 13);
+            this.Lbl_Nome.Name = "Lbl_Nome";
+            this.Lbl_Nome.Size = new System.Drawing.Size(74, 13);
+            this.Lbl_Nome.TabIndex = 0;
+            this.Lbl_Nome.Text = "Digite o Nome";
+            // 
+            // Lbl_Peso
+            // 
+            this.Lbl_Peso.AutoSize = true;
+            this.Lbl_Peso.Location = new System.Drawing.Point(13, 65);
+            this.Lbl_Peso.Name = "Lbl_Peso";
+            this.Lbl_Peso.Size = new System.Drawing.Size(70, 13);
+            this.Lbl_Peso.TabIndex = 1;
+            this.Lbl_Peso.Text = "Digite o Peso";
+            // 
+            // Lbl_Altura
+            // 
+            this.Lbl_Altura.AutoSize = true;
+            this.Lbl_Altura.Location = new System.Drawing.Point(13, 114);
+            this.Lbl_Altura.Name = "Lbl_Altura";
+            this.Lbl_Altura.Size = new System.Drawing.Size(73, 13);
+            this.Lbl_Altura.TabIndex = 2;
+            this.Lbl_Altura.Text = "Digite a Altura";
+            // 
+            // Lbl_Idade
+            // 
+            this.Lbl_Idade.AutoSize = true;
+            this.Lbl_Idade.Location = new System.Drawing.Point(13, 171);
+            this.Lbl_Idade.Name = "Lbl_Idade";
+            this.Lbl_Idade.Size = new System.Drawing.Size(73, 13);
+            this.Lbl_Idade.TabIndex = 3;
+            this.Lbl_Idade.Text = "Digite a Idade";
+            // 
+            // Lbl_Camisa
+            // 
+            this.Lbl_Camisa.AutoSize = true;
+            this.Lbl_Camisa.Location = new System.Drawing.Point(13, 230);
+            this.Lbl_Camisa.Name = "Lbl_Camisa";
+            this.Lbl_Camisa.Size = new System.Drawing.Size(80, 13);
+            this.Lbl_Camisa.TabIndex = 4;
+            this.Lbl_Camisa.Text = "Digite a Camisa";
+            // 
+            // Lbl_Time
+            // 
+            this.Lbl_Time.AutoSize = true;
+            this.Lbl_Time.Location = new System.Drawing.Point(13, 285);
+            this.Lbl_Time.Name = "Lbl_Time";
+            this.Lbl_Time.Size = new System.Drawing.Size(69, 13);
+            this.Lbl_Time.TabIndex = 5;
+            this.Lbl_Time.Text = "Digite o Time";
+            // 
+            // Txt_Nome
+            // 
+            this.Txt_Nome.Location = new System.Drawing.Point(184, 10);
+            this.Txt_Nome.Name = "Txt_Nome";
+            this.Txt_Nome.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Nome.TabIndex = 6;
+            // 
+            // Txt_Peso
+            // 
+            this.Txt_Peso.Location = new System.Drawing.Point(184, 58);
+            this.Txt_Peso.Name = "Txt_Peso";
+            this.Txt_Peso.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Peso.TabIndex = 7;
+            // 
+            // Txt_Altura
+            // 
+            this.Txt_Altura.Location = new System.Drawing.Point(184, 107);
+            this.Txt_Altura.Name = "Txt_Altura";
+            this.Txt_Altura.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Altura.TabIndex = 8;
+            // 
+            // Txt_Idade
+            // 
+            this.Txt_Idade.Location = new System.Drawing.Point(184, 164);
+            this.Txt_Idade.Name = "Txt_Idade";
+            this.Txt_Idade.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Idade.TabIndex = 9;
+            // 
+            // Txt_Camisa
+            // 
+            this.Txt_Camisa.Location = new System.Drawing.Point(184, 223);
+            this.Txt_Camisa.Name = "Txt_Camisa";
+            this.Txt_Camisa.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Camisa.TabIndex = 10;
+            // 
+            // Txt_Time
+            // 
+            this.Txt_Time.Location = new System.Drawing.Point(184, 278);
+            this.Txt_Time.Name = "Txt_Time";
+            this.Txt_Time.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Time.TabIndex = 11;
+            // 
+            // Btn_Salvar
+            // 
+            this.Btn_Salvar.Location = new System.Drawing.Point(575, 65);
+            this.Btn_Salvar.Name = "Btn_Salvar";
+            this.Btn_Salvar.Size = new System.Drawing.Size(145, 23);
+            this.Btn_Salvar.TabIndex = 12;
+            this.Btn_Salvar.Text = "Salvar";
+            this.Btn_Salvar.UseVisualStyleBackColor = true;
+            this.Btn_Salvar.Click += new System.EventHandler(this.Btn_Salvar_Click);
+            // 
+            // Btn_Pesquisa
+            // 
+            this.Btn_Pesquisa.Location = new System.Drawing.Point(575, 219);
+            this.Btn_Pesquisa.Name = "Btn_Pesquisa";
+            this.Btn_Pesquisa.Size = new System.Drawing.Size(145, 23);
+            this.Btn_Pesquisa.TabIndex = 13;
+            this.Btn_Pesquisa.Text = "Pesquisar Jogador";
+            this.Btn_Pesquisa.UseVisualStyleBackColor = true;
+            this.Btn_Pesquisa.Click += new System.EventHandler(this.Btn_Pesquisa_Click);
+            // 
+            // Txt_Pesquisar
+            // 
+            this.Txt_Pesquisar.Location = new System.Drawing.Point(575, 278);
+            this.Txt_Pesquisar.Name = "Txt_Pesquisar";
+            this.Txt_Pesquisar.Size = new System.Drawing.Size(145, 20);
+            this.Txt_Pesquisar.TabIndex = 14;
+            // 
+            // Frm_Jogador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Txt_Pesquisar);
+            this.Controls.Add(this.Btn_Pesquisa);
+            this.Controls.Add(this.Btn_Salvar);
+            this.Controls.Add(this.Txt_Time);
+            this.Controls.Add(this.Txt_Camisa);
+            this.Controls.Add(this.Txt_Idade);
+            this.Controls.Add(this.Txt_Altura);
+            this.Controls.Add(this.Txt_Peso);
+            this.Controls.Add(this.Txt_Nome);
+            this.Controls.Add(this.Lbl_Time);
+            this.Controls.Add(this.Lbl_Camisa);
+            this.Controls.Add(this.Lbl_Idade);
+            this.Controls.Add(this.Lbl_Altura);
+            this.Controls.Add(this.Lbl_Peso);
+            this.Controls.Add(this.Lbl_Nome);
+            this.Name = "Frm_Jogador";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Frm_Jogador_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Lbl_Nome;
+        private System.Windows.Forms.Label Lbl_Peso;
+        private System.Windows.Forms.Label Lbl_Altura;
+        private System.Windows.Forms.Label Lbl_Idade;
+        private System.Windows.Forms.Label Lbl_Camisa;
+        private System.Windows.Forms.Label Lbl_Time;
+        private System.Windows.Forms.TextBox Txt_Nome;
+        private System.Windows.Forms.TextBox Txt_Peso;
+        private System.Windows.Forms.TextBox Txt_Altura;
+        private System.Windows.Forms.TextBox Txt_Idade;
+        private System.Windows.Forms.TextBox Txt_Camisa;
+        private System.Windows.Forms.TextBox Txt_Time;
+        private System.Windows.Forms.Button Btn_Salvar;
+        private System.Windows.Forms.Button Btn_Pesquisa;
+        private System.Windows.Forms.TextBox Txt_Pesquisar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula5_RA6322501
+{
+    public partial class Frm_Jogador : Form
+    {
+        public Frm_Jogador()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_Jogador_Load(object sender, EventArgs e)
+        {
+
+        }
+        Jogador player;
+        Jogador[] vetorJogador = new Jogador[3];
+        int i = 0;
+
+        private void Btn_Salvar_Click(object sender, EventArgs e)
+        {
+            if (i < 3)
+            {
+                player = new Jogador();
+                player.Nome = Txt_Nome.Text;
+                player.Peso = Convert.ToDouble (Txt_Peso.Text);
+                player.Altura = Convert.ToDouble(Txt_Altura.Text);
+                player.Idade = Convert.ToInt32(Txt_Idade.Text);
+                player.Camisa = Convert.ToInt32(Txt_Camisa.Text);
+                player.Time = Txt_Time.Text;
+                vetorJogador[i] = player;
+                MessageBox.Show("Jogador Salvo.");
+                Txt_Nome.Text = "";
+                Txt_Peso.Text = "";
+                Txt_Altura.Text = "";
+                Txt_Idade.Text = "";
+                Txt_Camisa.Text = "";
+                Txt_Time.Text = "";
+                i++;
+            }
+            else
+            {
+                MessageBox.Show("Não é possível salvar esses dados.");
+            }
+        }
+
+        private void Btn_Pesquisa_Click(object sender, EventArgs e)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (vetorJogador[j].Nome.Equals(Txt_Pesquisar.Text))
+                {
+                    Txt_Nome.Text = vetorJogador[j].Nome;
+                    Txt_Peso.Text = vetorJogador[j].Peso.ToString();
+                    Txt_Altura.Text = vetorJogador[j].Altura.ToString();
+                    Txt_Idade.Text = vetorJogador[j].Idade.ToString();
+                    Txt_Camisa.Text = vetorJogador[j].Camisa.ToString();
+                    Txt_Time.Text = vetorJogador[j].Time;
+                    j = 2;
+                    Btn_Pesquisa.Text = "Outra pesquisa?";
+                    Txt_Pesquisar.Text = "";
+                }
+                else
+                {
+                    if (j == 2)
+                    {
+                        MessageBox.Show("Jogador não encontrado!");
+                        Btn_Pesquisa.Text = "Outra pesquisa?";
+                        Txt_Pesquisar.Text = "";
+                    }
+                }
+            }
+        }
+    }
+}
